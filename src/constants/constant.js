00739062b27c5182190b24947c7a25bb1b440b41
Dharm Veer <109572838+dharmveerofficial@users.noreply.github.com>
2024-01-16 11:49:15 +0530

    10-01-2024

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://192.168.48.25/api/';
-export const News_URL = 'http://192.168.48.25';
+// export const BASE_URL = 'http://192.168.48.25/api/';
+// export const News_URL = 'http://192.168.48.25';
 
-//  export const BASE_URL = 'https://impacthisar.xyz/api/';
-//  export const News_URL = 'https://impacthisar.xyz';
+export const BASE_URL = 'https://impacthisar.xyz/api/';
+export const News_URL = 'https://impacthisar.xyz';
